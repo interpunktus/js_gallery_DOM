9ab369f09b968ca645fcc143701c377928339c5b
interpunktus <240490702+interpunktus@users.noreply.github.com>
2026-06-14 11:20:14 +0300

    make build

---
+++ index.314318a5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){var r=t.target.closest(".list-item__link");r&&(t.preventDefault(),e.src=r.href,e.alt=r.title)});
+//# sourceMappingURL=index.314318a5.js.map
